23f2a5aee538f10fae0a6702a495cb963a3730c6
DmitryAvdeenko <DmitryAvdeenko@gmail.com>
2023-02-19 21:40:37 +0100

    Added Intellij build system

---
+++ src/bubbles/SparklingWater.java
@@ -0,0 +1,50 @@
+/*
+package bubbles;
+
+public class SparklingWater extends Water {
+
+    public SparklingWater(String color, String transparent, String smell, double temprature) {
+        super(color, transparent, smell, temprature);
+    }
+
+    @Override
+    public void drink() {
+
+    }
+}
+
+    */
+/*private final Bubbles[] bubbles;
+
+    public SparklingWater(String color, String transparent, String smell, double temprature, Bubbles[] bubbles) {
+        super(color, transparent, smell, temprature);
+
+    }
+
+    public void pump(Bubble[] bubbles) {
+
+    }
+
+    public void degas() {
+        int bubblesToRemove = bubbles.length / 10;
+        for (int i = 0; i < bubblesToRemove; i++) {
+            bubbles[i].cramp();
+        }
+    }
+
+
+
+    @Override
+    public void drink() {
+*//*
+
+    */
+/*}
+}
+
+//- создать класс SparklingWater, являющийся дочерним Water
+//- у газировки есть пузырьки
+//- вода заполняется пузырьками при упаковке на заводе, для этого у нее есть метод pump(Bubble[] bubbles)
+//- 1 литр воды содержит 10 тыс пузырьков
+//- у газировки есть метод degas(), который удаляет пузырьки по одному и вызывает их лопанье
+*/
