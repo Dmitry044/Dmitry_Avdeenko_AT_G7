2a531eb0d1ecd7ef517ed648760ef383300e4521
DmitryAvdeenko <DmitryAvdeenko@gmail.com>
2023-02-21 21:40:31 +0100

    I've finished task Bubbles_Task_one

--- src/main/java/bubbles/Water.java
+++ src/main/java/bubbles/Water.java
@@ -2,52 +2,9 @@ package bubbles;
 
 public abstract class Water {
     private String color;
-    private String transparent;
+    private String clarity;
     private String smell;
-    private double temprature;
-
-    public Water(String color, String transparent, String smell, double temprature) {
-        this.color = color;
-        this.transparent = transparent;
-        this.smell = smell;
-        this.temprature = temprature;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getTransparent() {
-        return transparent;
-    }
-
-    public void setTransparent(String transparent) {
-        this.transparent = transparent;
-    }
-
-    public String getSmell() {
-        return smell;
-    }
-
-    public void setSmell(String smell) {
-        this.smell = smell;
-    }
-
-    public double getTemprature() {
-        return temprature;
-    }
-
-    public void setTemprature(double temprature) {
-        this.temprature = temprature;
-    }
-
-    public abstract void drink();
-
-
+    private double temperature;
 }
 
 //- создать абстрактный класс Water
