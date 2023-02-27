b0fb47b1666c4b5b6e828e5390f7b2c2f4f908d4
DmitryAvdeenko <DmitryAvdeenko@gmail.com>
2023-02-28 00:56:10 +0100

    Create_packages_and_classes

---
+++ src/homework/day4/playground/essence/creatures/playground/Bug.java
@@ -0,0 +1,6 @@
+package homework.day4.playground.essence.creatures.playground;
+
+import homework.day4.playground.essence.creatures.Insect;
+
+public class Bug extends Insect {
+}
---
+++ src/homework/day4/playground/essence/creatures/playground/Raven.java
@@ -0,0 +1,6 @@
+package homework.day4.playground.essence.creatures.playground;
+
+import homework.day4.playground.essence.creatures.Vertebrata;
+
+public class Raven extends Vertebrata {
+}
