5627a728fb5870fa0a85a45bea22d1bd24294192
DmitryAvdeenko <DmitryAvdeenko@gmail.com>
2023-02-28 23:06:00 +0100

    I've finished 2.1-2.3

--- src/homework/day4/playground/essence/craft/air/Aircraft.java
+++ src/homework/day4/playground/essence/craft/air/Aircraft.java
@@ -3,4 +3,19 @@ package homework.day4.playground.essence.craft.air;
 import homework.day4.playground.essence.Matter;
 
 public abstract class Aircraft extends Matter {
+
+    protected String name;
+
+    public Aircraft(int mass, String name) {
+        super(mass);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/homework/day4/playground/essence/craft/field/Vehicle.java
+++ src/homework/day4/playground/essence/craft/field/Vehicle.java
@@ -3,4 +3,19 @@ package homework.day4.playground.essence.craft.field;
 import homework.day4.playground.essence.Matter;
 
 public abstract class Vehicle extends Matter {
+
+    protected String name;
+
+    public Vehicle(int mass, String name) {
+        super(mass);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
