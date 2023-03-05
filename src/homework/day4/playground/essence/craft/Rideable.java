7fd186370a632675abea05ec9f2d64e8047161ce
DmitryAvdeenko <DmitryAvdeenko@gmail.com>
2023-03-05 23:51:21 +0100

    Done_task6_1

--- src/homework/day4/playground/essence/craft/Rideable.java
+++ src/homework/day4/playground/essence/craft/Rideable.java
@@ -2,4 +2,5 @@ package homework.day4.playground.essence.craft;
 
 public interface Rideable {
     void ride();
+    void drive(String direction);
 }
--- src/homework/day4/playground/essence/craft/field/Moped.java
+++ src/homework/day4/playground/essence/craft/field/Moped.java
@@ -4,4 +4,13 @@ public class Moped extends Vehicle{
     public Moped(int mass, String name) {
         super(mass, name);
     }
+
+    @Override
+    public void drive(String direction) {
+        System.out.printf("I am %s, my name is %s and I amd driving to %s", getClass().getSimpleName(), name, direction);
+
+        //принимает строку направления движения и печатает в консоль фразу "I am N, my name is M and
+        // I amd driving to D", где N - название класса соответствующего обьекта,
+        // М - значение name обьекта, D - строка, подставляемая из значения передаваемого аргумента
+    }
 }
