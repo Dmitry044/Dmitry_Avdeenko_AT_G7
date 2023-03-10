856ed6e993a7c32d512964994a223a4a8073b041
DmitryAvdeenko <DmitryAvdeenko@gmail.com>
2023-03-10 23:55:25 +0100

    String_task_printDublicateWorlds

---
+++ src/homework/day4/stringTask/StringObjectRunner.java
@@ -0,0 +1,13 @@
+package homework.day4.stringTask;
+
+public class StringObjectRunner {
+    public static void main(String[] args) {
+        PrintLetters.printLettersInColumn("Hello World");
+        PrintDuplicate.printDuplicateWords("kj test test");
+        PrintDuplicate.findDuplicatedUsingEqualsMethod("test new iu iu test Dima Dima");
+    }
+
+
+
+
+}
